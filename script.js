c563d1145064fe8cb6db00ec527fcb122af32b62
Nazneen Akter <nazneenzaman195@gmail.com>
2025-05-22 11:51:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// let btn = document.querySelector("button")
+
+// btn.addEventListener("click", function(){
+//     document.body.style.background="red"
+// })
+
+// ====================
+function teal() {
+    document.body.style.background = "tomato"
+}
